d6e1acff580cfde016bab060384785d7b67af2c8
jbmendoza <johndale.mendoza@gmail.com>
2024-07-26 20:53:01 +0800

    Test Model

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "has a valid factory" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = build(:user, email: nil)
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without a password" do
+    user = build(:user, password: nil)
+    expect(user).not_to be_valid
+  end
+end
