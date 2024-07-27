05bd4f1c5370976ccc44991d927761e1a9d79515
Eljan Peralta <eljan233@gmail.com>
2024-07-27 12:18:56 +0800

    Added View and Edit details for Admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   
     private
   
-    # Checks if the current user is an admin and redirects if not
     def authorize_admin!
       authenticate_user!
       unless current_user&.admin?
@@ -11,14 +10,13 @@ class ApplicationController < ActionController::Base
       end
     end
   
-    # Customizes redirection after user sign-in
     def after_sign_in_path_for(resource)
       if resource.user_type == 'trader'
         trader_home_path
       elsif resource.user_type = 'admin'
         admin_users_path  
       else
-        root_path # Redirect others to the default root path
+        root_path
       end
     end
 end
