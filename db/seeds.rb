f24434f3d57a71be6ab2c1925f9c72ffcd491e43
jbmendoza <johndale.mendoza@gmail.com>
2024-07-29 19:57:20 +0800

    Added a User admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(email: 'admin@email.com', password: 'admin123pass', user_type: 'admin')
