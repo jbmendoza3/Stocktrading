c545070f267789505d558dc344431c592b0d0c99
jbmendoza <johndale.mendoza@gmail.com>
2024-08-02 17:11:20 +0800

    Added conditions for pending and approval email and change the Test Model

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem 'factory_bot_rails'
+  gem 'shoulda-matchers', '~> 5.0'
 
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,15 @@ require './spec/support/factory_bot'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+require 'shoulda/matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
